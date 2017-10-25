8b61787c295612a2710318e1926c34f6258c7abb
Jesper Haug Karsrud <jesper.karsrud@gmail.com>
2017-10-25 23:50:36 +0200

    Add content test to <Link /> component

--- src/ui/components/Link/component-test.ts
+++ src/ui/components/Link/component-test.ts
@@ -10,4 +10,11 @@ module('Component: Link', function(hooks) {
     await this.render(hbs`<Link />`);
     assert.ok(this.containerElement.querySelector('a'));
   });
+
+  test('it renders the content', async function(assert) {
+    const text = 'Test';
+    await this.render(hbs`<Link @to="/test">Test</Link>`);
+    assert.ok(this.containerElement.querySelector('a').href.includes('/test'));
+    assert.equal(this.containerElement.textContent.trim(), text);
+  });
 });
